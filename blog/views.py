87057ec690991e784c39c7b4077aec907266c163
Yisusad <jesuscheoz@gmail.com>
2022-04-14 01:27:45 -0400

    Heredando html

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 #Models
 from .models import Post
 
 # Create your views here.
 def posts(request):
     blogs = Post.objects.all()
-    return HttpResponse(blogs)
+    return render(request, 'blogs.html')
 
 def post(request, id):
     blog = Post.objects.get(id=id)
     content = f'{blog.title} - {blog.desc}'
-    return HttpResponse(content)
+    return render(request, 'blog.html')
--- perfil/views.py
+++ perfil/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 #Models
 from .models import Project
@@ -7,15 +6,6 @@ from .models import Project
 # Create your views here.
 def profile(request):
     
-    # p1 = Project(title = "Curso de Html", desc = "Descripción de Html")
-    # p1.save()
-    
-    # p2 = Project(title = "Curso de Css", desc = "Descripción de Css")
-    # p2.save()
-    
-    # p3 = Project(title = "Curso de Django", desc = "Descripción de Django")
-    # p3.save()
-    
     projects = Project.objects.all()
 
-    return HttpResponse(projects);
+    return render(request, 'profile.html');
